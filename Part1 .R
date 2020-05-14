23958f3234858918ac2fa63ccdccb5787dfda475
Your Name <student06@dee2-dataprocess1.novalocal>
2020-05-14 11:27:56 +1000

    coding for t.test and wilcox.test functions are completed.

--- Part1 .R
+++ Part1 .R
@@ -26,7 +26,7 @@ x$Mean <- as.matrix(x)
 hist(x$Mean)
 hist(as.matrix(x$Mean),10)
 
-#part2
+
 
 download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/growth_data.csv", 
               destfile = "try2.csv")
@@ -46,10 +46,30 @@ sd(southwest$Circumf_2004_cm)
 mean(southwest$Circumf_2019_cm)
 sd(southwest$Circumf_2019_cm)
 
-plot(southwest$Circumf_2004_cm,southwest$Circumf_2019_cm)
-plot(northeast$Circumf_2004_cm,northeast$Circumf_2019_cm)
+boxplot(southwest$Circumf_2004_cm,southwest$Circumf_2019_cm,northeast$Circumf_2004_cm,northeast$Circumf_2019_cm,
+        names = c("SW2004","SW2019","NE2004","NE2019"),ylab="Cirumference (cm)",
+        main="Growth at Two Plantation Sites")
 
-y$mean_growth <- rowMeans(y[,c("Circumf_2009_cm","Circumf_2014_cm","Circumf_2019_cm")])
-y
+Mean_growthSW <- (southwest$Circumf_2019_cm-southwest$Circumf_2009_cm)      
+Mean_growthNE <- (northeast$Circumf_2019_cm-northeast$Circumf_2009_cm)
+mean(Mean_growthSW)
+a <- mean(Mean_growthSW)
+mean(Mean_growthNE)
+b <- mean(Mean_growthNE)
+head(y)
 
-?t.test
+res <- t.test(Mean_growthSW,Mean_growthNE, var.equal = TRUE)
+res
+
+res <- wilcox.test(a,b)
+res
+
+
+
+#part 2
+
+download.file("http://dee2.io/tmp/SARS-CoV-2-seqs.fa.gz",destfile = "SARS-CoV-2-seqs.fa.gz")
+# uncompress the file
+R.utils::gunzip("SARS-CoV-2-seqs.fa.gz",overwrite=TRUE)
+# create the blast DB
+makeblastdb("SARS-CoV-2-seqs.fa",dbtype="nucl", "-parse_seqids")
